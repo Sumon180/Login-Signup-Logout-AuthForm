bd62cbefbed844f8abf7c741ba39b6d5744c6b44
sumon-hossain <sumonengr.96@gmail.com>
2023-06-10 18:09:13 +0600

    Wip

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import jwt, { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
